021259d1887716fca0b50c5b73696af57aee192e
xshengh <271736642@qq.com>
2015-10-07 23:32:48 +0800

    Create Contains Duplicate.java

---
+++ Contains Duplicate.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (numSet.contains(nums[i])) {
+                return true;
+            }
+            numSet.add(nums[i]);
+        }
+        return false;
+    }
+}
